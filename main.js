ac4ae186ac8638c67825198149b370c2074d9b4f
Michael Passade <michael.passade@codeimmersives.com>
2020-03-04 12:32:59 -0500

    done

--- main.js
+++ main.js
@@ -1,9 +1,54 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const call = function(func){
+  func()
+}
 
+const callTwice = function(func){
+  func()
+  func()
+}
 
+const callXTimes = function(func, num){
+  for (let i=0; i<num; i++){
+    func()
+  }
+}
 
+const returnFromFunc = function(func){
+  return func()
+}
+
+const modifyString = function(str, func){
+  return func(str)
+}
+
+const modifyNumber = function(num, func){
+  return func(num)
+}
+
+const modifyAnything = function(x, func){
+  return func(x)
+}
+
+const twoFuncs = function(func1, func2){
+  const x = func1()
+  return func2(x)
+}
+
+const threeFuncs = function(func1,func2,func3){
+  const x = twoFuncs(func1,func2)
+  return func3(x)
+}
+
+const twoValues = function(val1,val2,func){
+  return func(val1,val2)
+}
+
+const twoValuesRTL = function(val1,val2,func){
+  return func(val2,val1)
+}
 
 /********************************
  * OUR CODE HERE - DON'T TOUCH! *
@@ -63,6 +108,7 @@ module.exports = {
   modifyNumber,
   modifyAnything,
   twoFuncs,
+  threeFuncs,
   twoValues,
   twoValuesRTL,
 }
